0ff5ab569c98849e692c88cafab1a082937772f6
Mark Newman <mwnewman@umich.edu>
2025-11-23 23:25:02 -0500

    added maps

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { 
+  Accuracy,
+  requestForegroundPermissionsAsync,
+  watchPositionAsync 
+} from 'expo-location';
+
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
+
+  const initRegion = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }
+
+  const [location, setLocation] = useState(null);
+  const [permissionsGranted, setPermissionsGranted] = useState(false);
+  const [mapRegion, setMapRegion] = useState(initRegion);
+
+  let unsubscribeFromLocation = null;
+
+  const subscribeToLocation = async () => {
+
+    let { status } = await requestForegroundPermissionsAsync();
+    setPermissionsGranted(status === 'granted');
+
+    if (unsubscribeFromLocation) {
+      unsubscribeFromLocation();
+    }
+    unsubscribeFromLocation = await watchPositionAsync({
+      accuracy: Accuracy.Highest,
+      distanceInterval: 1, // 1 meter
+      timeInterval: 1000 // 1000ms = 1s
+    }, location => {
+      console.log('received update:', location);
+      setLocation(location);
+      setMapRegion({
+        ...mapRegion,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      })
+    });
+  }
+
+  useEffect(() => {
+    subscribeToLocation();
+  }, []);
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={styles.paragraph}>
+        {permissionsGranted ?
+          location ?
+            `lat: ${location.coords.latitude} \n` + 
+            `lon: ${location.coords.longitude}`
+          :
+            "Waiting..."
+        :
+          "Location permission not granted."
+        }
+      </Text>
+
+      <MapView 
+        style={styles.map} 
+        provider={PROVIDER_GOOGLE} 
+        region={mapRegion}  
+        showsUserLocation={true}
+      /> 
+     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  paragraph: {
+    fontSize: 24
+  },
+  map: {
+    flex: 0.5,
+    width: '100%',
+    height: '100%'
+  }
+})
